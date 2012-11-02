04bbee45264f43ca6b79e68c946c25019550dafe
n3mo <nemo1211@gmail.com>
2012-11-02 01:24:30 -0400

    Linux/OSX compatibility tweaks

--- custom.el
+++ custom.el
@@ -20,3 +20,9 @@
 ;;  ;; Your init file should contain only one such instance.
 ;;  ;; If there is more than one, they won't work right.
 ;;  '(default ((t (:family "Monaco" :foundry "unknown" :slant normal :weight normal :height 98 :width normal)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal)))))
--- mac.el
+++ mac.el
@@ -1,59 +1,61 @@
 ;; Customizations unique to running Emacs on OS X
 
-;; Change command to meta, and ignore option so that special
-;; characters can be entered with option
-(setq mac-option-modifier 'none)
-(setq mac-command-modifier 'meta)
+(if (eq system-type 'darwin)
+    (progn
+      ;; Change command to meta, and ignore option so that special
+      ;; characters can be entered with option
+      (setq mac-option-modifier 'none)
+      (setq mac-command-modifier 'meta)
 
-;; Emacs for mac os x doesn't find my bash PATH, so I've specified it
-;; here manually. There's probably a more elegent way of doing this. I
-;; noticed Magnars (Emacs Rocks!) does something in his setup file
-;; mac.el
-(setenv "PATH"
-	(concat
-	 "/Users/nemo/bin" ":"
-	 "/opt/subversion/bin" ":"
-	 "/sw/bin" ":"
-	 "/sw/sbin" ":"
-	 "/bin" ":"
-	 "/sbin" ":" 
-	 "/usr/bin" ":" 
-	 "/usr/sbin" ":"
-	 "/usr/local/bin" ":"
-	 "/usr/texbin" ":"
-	 "~/Library/Haskell/bin"
-	 ))
+      ;; Emacs for mac os x doesn't find my bash PATH, so I've specified it
+      ;; here manually. There's probably a more elegent way of doing this. I
+      ;; noticed Magnars (Emacs Rocks!) does something in his setup file
+      ;; mac.el
+      (setenv "PATH"
+	      (concat
+	       "/Users/nemo/bin" ":"
+	       "/opt/subversion/bin" ":"
+	       "/sw/bin" ":"
+	       "/sw/sbin" ":"
+	       "/bin" ":"
+	       "/sbin" ":" 
+	       "/usr/bin" ":" 
+	       "/usr/sbin" ":"
+	       "/usr/local/bin" ":"
+	       "/usr/texbin" ":"
+	       "~/Library/Haskell/bin"
+	       ))
 
-;; Specifying the path to the ghostscript binary seems nessary to get
-;; doc-view to work. This problem (and other "Emacs for Mac OS X" path
-;; problems) can also be fixed by launchin Emacs.app from the
-;; terminal, rather than from the doc or finder. Launching emacs.app
-;; in that way ensures my full paths are loaded properly.
-(setq exec-path (append exec-path '("/sw/bin")))
-(setq exec-path (append exec-path '("/Users/nemo/bin")))
-;; Specify git binaries
-(setq exec-path (append exec-path '("/usr/local/bin")))
+      ;; Specifying the path to the ghostscript binary seems nessary to get
+      ;; doc-view to work. This problem (and other "Emacs for Mac OS X" path
+      ;; problems) can also be fixed by launchin Emacs.app from the
+      ;; terminal, rather than from the doc or finder. Launching emacs.app
+      ;; in that way ensures my full paths are loaded properly.
+      (setq exec-path (append exec-path '("/sw/bin")))
+      (setq exec-path (append exec-path '("/Users/nemo/bin")))
+      ;; Specify git binaries
+      (setq exec-path (append exec-path '("/usr/local/bin")))
 
-;; This adds support for using OS X's address book database in
-;; GNUS. This allows you to start typing someone's name in an a
-;; message header and then press C-c TAB to complete their email
-;; address. It relied on the command line tool "contacts" which I
-;; installed to provide command line interaction the address book. 
-;; I have since adopted use of the Insidious Big Brother Database, so
-;; I am unlikely to need this slower option anymore. However, my OS X
-;; address book has different contacts and I could conceivably need
-;; them down the road, so this hack remains open
-(require 'external-abook)
-(setq external-abook-command "contacts -lSf '%%e\t\"%%n\"' '%s'")
-(eval-after-load "message" 
-  '(progn 
-     (add-to-list 'message-mode-hook 
-		  '(lambda ()
-		     (define-key message-mode-map "\C-c\t"
-		       'external-abook-try-expand)))))
+      ;; This adds support for using OS X's address book database in
+      ;; GNUS. This allows you to start typing someone's name in an a
+      ;; message header and then press C-c TAB to complete their email
+      ;; address. It relied on the command line tool "contacts" which I
+      ;; installed to provide command line interaction the address book. 
+      ;; I have since adopted use of the Insidious Big Brother Database, so
+      ;; I am unlikely to need this slower option anymore. However, my OS X
+      ;; address book has different contacts and I could conceivably need
+      ;; them down the road, so this hack remains open
+      (require 'external-abook)
+      (setq external-abook-command "contacts -lSf '%%e\t\"%%n\"' '%s'")
+      (eval-after-load "message" 
+	'(progn 
+	   (add-to-list 'message-mode-hook 
+			'(lambda ()
+			   (define-key message-mode-map "\C-c\t"
+			     'external-abook-try-expand)))))
 
-;; Move to trash when deleting stuff
-(setq delete-by-moving-to-trash t
-      trash-directory "~/.Trash/emacs")
-
+      ;; Move to trash when deleting stuff
+      (setq delete-by-moving-to-trash t
+	    trash-directory "~/.Trash/emacs")))
+      
 (provide 'mac)
