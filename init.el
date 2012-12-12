9b88bf79625bb97c4e489ea8e6ef814fe703c0a0
Nick Van Horn <nemo1211@gmail.com>
2012-12-12 16:39:35 -0500

    Various customizations for chicken scheme

--- init.el
+++ init.el
@@ -305,7 +305,7 @@
 ;; Here are the Emacs lisp repositories that I draw from (for
 ;; packages-list-packages, etc)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")
 			 ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 ;; This is necessary for bibtex-to-plain-text to work
@@ -361,17 +361,43 @@
 (add-hook 'scheme-mode-hook 'paredit-mode)
 (add-hook 'clojure-mode-hook 'paredit-mode)
 
+;; I REALLY like geiser-mode for editing scheme. Unfortunately, it
+;; only works with Guile and Racket. This is frustrating when I'm
+;; editing (for example) Chicken scheme and Geiser-mode keeps
+;; activating. Setting this to nil ensures that geiser-mode does not
+;; activate automatically
+(setq geiser-mode-auto-p nil)
+
 ;; I've been experimenting with various flavors of scheme. This sets
 ;; the default binary to run when an inferior scheme lisp repl is
 ;; called 
-;; (setq scheme-program-name "csi -:c")  ;; Chicken scheme
-(setq scheme-program-name "racket")  ;; Racketscheme
+(setq scheme-program-name "csi -:c")  ;; Chicken scheme
+;; (setq scheme-program-name "racket")  ;; Racketscheme
 
 ;; I'm experimenting with various chicken-scheme modes. Currently, I'm
 ;; using cluck, which is a chicken-specific fork of quack-mode:
 ;; https://github.com/ddp/cluck
 ;; (add-to-list 'load-path "~/.emacs.d/plugins/cluck/")
 ;; (require 'cluck)
+;; (require 'quack)
+
+;; Chicken-doc customization for finding documentation of sexp at
+;; point
+(defun chicken-doc (&optional obtain-function)
+  (interactive)
+  (let ((func (funcall (or obtain-function 'current-word))))
+    (when func
+      (process-send-string (scheme-proc)
+                           (format "(require-library chicken-doc) ,doc %S\n" func))
+      (save-selected-window
+        (select-window (display-buffer (get-buffer scheme-buffer) t))
+        (goto-char (point-max))))))
+
+(eval-after-load 'cmuscheme
+  '(define-key scheme-mode-map "\C-cd" 'chicken-doc))
+(eval-after-load 'cmuscheme
+ '(define-key inferior-scheme-mode-map "\C-cd"
+    (lambda () (interactive) (chicken-doc 'sexp-at-point))))
 
 ;; This sets the default common lisp program
 (setq inferior-lisp-program "sbcl")
