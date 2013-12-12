251a0e46c5c65d156eef4ab73a391f4666bf2122
n3mo <nemo1211@gmail.com>
2013-12-12 18:39:45 -0500

    Added custom htmlize function for blog-worthy code

--- plugins/my-functions.el
+++ plugins/my-functions.el
@@ -8,6 +8,32 @@
 ;;       Assorted Utility Functions   ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; This function takes a region of code and produces html with
+;; embedded css suitable for pasting into my markdown-->clojure-->html
+;; site generator for creating posts at nicholasvanhorn.com. Because
+;; htmlize is used, the current theme determines the css coloring. 
+(defun my-htmlize-region (beg end)
+  "Htmlize region and put into <pre> tag style that is left in <body> tag
+plus add font-size: 8pt"
+  (interactive "r")
+  (let* ((buffer-faces (htmlize-faces-in-buffer))
+         (face-map (htmlize-make-face-map (adjoin 'default buffer-faces)))
+         (pre-tag (format
+                   "<pre style=\"%s font-size: 8pt\">"
+                   (mapconcat #'identity (htmlize-css-specs
+                                          (gethash 'default face-map)) " ")))
+         (htmlized-reg (htmlize-region-for-paste beg end)))
+    (switch-to-buffer-other-window "*htmlized output*")
+    ; clear buffer
+    (kill-region (point-min) (point-max))
+    ; set mode to have syntax highlighting
+    (nxml-mode)
+    (save-excursion
+      (insert htmlized-reg))
+    (while (re-search-forward "<pre>" nil t)
+      (replace-match pre-tag nil nil))
+    (goto-char (point-min))))
+
 ;; This function evaluates the sexp before point (similar to
 ;; eval-last-sexp) and replaces the sexp with its result.
 (defun replace-last-sexp ()
