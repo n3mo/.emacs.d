05d4862f3f0e987ec455eba0f94a6deba364471e
n3mo <nemo1211@gmail.com>
2014-09-10 11:39:37 -0400

    Updated to latest org mode

--- setup-org-mode.el
+++ setup-org-mode.el
@@ -11,7 +11,7 @@
 ;; built-in version. This is a shame, as every time I update org-mode
 ;; with package.el, I will need to manually change the load-path
 ;; below. 
-(add-to-list 'load-path "~/.emacs.d/elpa/org-20140630/")
+(add-to-list 'load-path "~/.emacs.d/elpa/org-20140908")
 ;; (require 'org-install) ;; This also shouldn't be necessary...
 
 ;; The local-unset-key in this hook removes the org-mode binding that
