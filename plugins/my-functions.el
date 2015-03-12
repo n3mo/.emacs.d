a7769dfbdf4dbaa73585b4fb0c38c1310ed59af8
nemo <nemo1211@gmail.com>
2015-03-11 23:46:10 -0400

    underline-text now properly handles tabs

--- plugins/my-functions.el
+++ plugins/my-functions.el
@@ -166,38 +166,49 @@ C-u', does not underline whitespace embedded in the line."
            (if (= arg 1)
                "="
              (char-to-string
-	       (read-char "What character to underline with?")))))
-	 (original-point-is-eol
-	  (when (looking-at "$") t))
-	 (original-point-is-eob (= original-point (point-max))))
+           (read-char "What character to underline with?")))))
+         (original-point-is-eol
+           (when (looking-at "$") t))
+         (original-point-is-eob
+           (= original-point (point-max))))
     (beginning-of-line)
     (unless
-      (when (looking-at "[ \t]*$")
+      (when (looking-at "[[:space:]]*$")
         (beginning-of-line 0)
-        (when (looking-at "[ \t]*$")
+        (when (looking-at "[[:space:]]*$")
           (goto-char original-point)
           (message "nothing to do")))
-    (insert (buffer-substring (line-beginning-position) (line-end-position)) "\n")
-    (save-restriction
-      (narrow-to-region
-        (re-search-forward
-	  "\\([[:space:]]*#+\\)\\|\\([[:space:]]*;+\\)\\|\\([[:space:]]*//\\)\\|\\([[:space:]]*/\\*\\)\\|[[:space:]]*" nil t)
-	(1+ (progn (goto-char (line-end-position))
-	           (re-search-backward "[^[:space:]]" nil t))))
-      (goto-char (point-min))
-      (if (= arg 16)
-        (while (re-search-forward "[^[:space:]]" nil t)
-          (replace-match underline-char nil))
-       (re-search-forward "[^[:space:]]" nil t)
-       (goto-char (1- (point)))
-       (while (re-search-forward "." nil t)
-         (replace-match underline-char nil)))
-       (widen))
-    (if original-point-is-eob
-	(goto-char (point-max))
-      (if original-point-is-eol
-	  (goto-char (re-search-forward "^"))
-	(goto-char original-point))))))
+      (insert
+        (buffer-substring (line-beginning-position) (line-end-position))
+        "\n")
+      (save-restriction
+        (narrow-to-region
+          (progn
+            (goto-char (1- (re-search-forward "[^[:space:]]" nil t)))
+            (cond
+              ((looking-at ";+")   (match-end 0))
+              ((looking-at "#+")   (match-end 0))
+              ((looking-at "//+")  (match-end 0))
+              ((looking-at "/\\*+") (match-end 0))
+              (t (point))))
+          (1+ (progn
+	    (goto-char (line-end-position))
+            (re-search-backward "[^[:space:]]" nil t))))
+        (untabify (point-min) (point-max))
+        (goto-char (point-min))
+        (if (= arg 16)
+          (while (re-search-forward "[^[:space:]]" nil t)
+            (replace-match underline-char nil))
+         (re-search-forward "[^[:space:]]" nil t)
+         (goto-char (1- (point)))
+         (while (re-search-forward "." nil t)
+           (replace-match underline-char nil)))
+        (widen))
+      (if original-point-is-eob
+        (goto-char (point-max))
+       (if original-point-is-eol
+         (goto-char (re-search-forward "^"))
+        (goto-char original-point))))))
 
 
 
